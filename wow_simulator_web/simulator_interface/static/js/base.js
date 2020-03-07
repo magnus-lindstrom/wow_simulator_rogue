d2d4cda1468d54d85b8ae4b9aa12a0ed374e5923
eugeniaspano <eugenia.spano@gmail.com>
2020-03-07 20:35:44 +0100

    added animation for confirmation message, added todo list in views

---
+++ wow_simulator_web/simulator_interface/static/js/base.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    var message_elem = document.getElementById("message-container");
+
+    setTimeout(function(){
+       message_elem.style.display = "none";
+    }, 3000);
+});
--- wow_simulator_web/simulator_interface/views.py
+++ wow_simulator_web/simulator_interface/views.py
@@ -6,8 +6,14 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    # def get(self, request, *args, **kwargs):
+        # fetch config files
+        # parse config
+        # create dictionary {slot_type: [item1, item2, ...]}
+
     def post(self, request, *args, **kwargs):
         head_item = request.POST.get('drop')
+        # todo: how to get ALL selected items and dropdown labels dinamically?
         messages.add_message(request, messages.SUCCESS, 'Form submitted correctly')
 
         response = {
