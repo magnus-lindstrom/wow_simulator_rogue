2fdb4dfcc1221c1e38be16c8baa833621470b3e0
eugeniaspano <eugenia.spano@gmail.com>
2020-03-07 19:58:29 +0100

    dropdown menu and confirmation message

--- wow_simulator_web/simulator_interface/views.py
+++ wow_simulator_web/simulator_interface/views.py
@@ -1,6 +1,16 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def post(self, request, *args, **kwargs):
+        head_item = request.POST.get('drop')
+        messages.add_message(request, messages.SUCCESS, 'Form submitted correctly')
+
+        response = {
+            'items': head_item,
+        }
+        return render(request, self.template_name, context=response)
