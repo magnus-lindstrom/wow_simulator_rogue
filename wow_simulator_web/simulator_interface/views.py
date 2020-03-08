ef0abe3203eb8f752cd4f1ac77e7e5abfcd599cf
eugeniaspano <eugenia.spano@gmail.com>
2020-03-08 12:39:45 +0100

    resized to get entire screen

--- wow_simulator_web/simulator_interface/views.py
+++ wow_simulator_web/simulator_interface/views.py
@@ -6,12 +6,30 @@ import yaml
 
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 ARMOR_FILE = os.path.join(REPO_ROOT, 'db', 'armor.yaml')
+ENCHANTS_FILE = os.path.join(REPO_ROOT, 'db', 'enchants.yaml')
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get(self, request, *args, **kwargs):
+        dropdown_armor_items = self._parse_armor_file()
+        context = {'armor': dropdown_armor_items}
+
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        head_item = request.POST.get('drop')
+        # todo: how to get ALL selected items and dropdown labels dinamically?
+        messages.add_message(request, messages.SUCCESS, 'Form submitted correctly')
+
+        response = {
+            'items': head_item,
+        }
+        return render(request, self.template_name, context=response)
+
+    @staticmethod
+    def _parse_armor_file():
         with open(ARMOR_FILE, 'r') as armor_file:
             content = yaml.load(armor_file, yaml.FullLoader)
 
@@ -23,19 +41,8 @@ class HomeView(TemplateView):
                 slots[slot_name].append(item_name)
             else:
                 slots[slot_name] = [item_name]
+        return slots
 
-        context = {'armor': slots}
-
-        return render(request, self.template_name, context=context)
-        # parse config
-        # create dictionary {slot_type: [item1, item2, ...]}
-
-    def post(self, request, *args, **kwargs):
-        head_item = request.POST.get('drop')
-        # todo: how to get ALL selected items and dropdown labels dinamically?
-        messages.add_message(request, messages.SUCCESS, 'Form submitted correctly')
-
-        response = {
-            'items': head_item,
-        }
-        return render(request, self.template_name, context=response)
+    @staticmethod
+    def _parse_enchants_file():
+        pass
