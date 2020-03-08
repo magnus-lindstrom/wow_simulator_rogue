04956e16c34c34c26ea71e55b25f2f0a986a9867
eugeniaspano <eugenia.spano@gmail.com>
2020-03-08 12:23:37 +0100

    parsed dropdowns dynamically

--- wow_simulator_web/simulator_interface/views.py
+++ wow_simulator_web/simulator_interface/views.py
@@ -1,13 +1,32 @@
 from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import os
+import yaml
+
+REPO_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+ARMOR_FILE = os.path.join(REPO_ROOT, 'db', 'armor.yaml')
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-    # def get(self, request, *args, **kwargs):
-        # fetch config files
+    def get(self, request, *args, **kwargs):
+        with open(ARMOR_FILE, 'r') as armor_file:
+            content = yaml.load(armor_file, yaml.FullLoader)
+
+        slots = {}
+        for item in content:
+            slot_name = content[item]['slot']
+            item_name = content[item]['name']
+            if slot_name in slots:
+                slots[slot_name].append(item_name)
+            else:
+                slots[slot_name] = [item_name]
+
+        context = {'armor': slots}
+
+        return render(request, self.template_name, context=context)
         # parse config
         # create dictionary {slot_type: [item1, item2, ...]}
 
